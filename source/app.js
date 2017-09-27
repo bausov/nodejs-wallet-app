aa6ee7c0f2dae0b4179496042f01ca7e48db2602
GreenNun <stas909@gmail.com>
2017-09-27 23:40:31 +0300

    Initial commit

---
+++ source/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Koa = require('koa');
+const serve = require('koa-static');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser')();
+
+const getCardsController = require('./controllers/cards/get-cards');
+const createCardController = require('./controllers/cards/create');
+const deleteCardController = require('./controllers/cards/delete');
+
+const errorController = require('./controllers/error');
+
+const ApplicationError = require('libs/application-error');
+const CardsModel = require('source/models/cards');
+
+const app = new Koa();
+
+// Сохраним параметр id в ctx.params.id
+router.param('id', (id, ctx, next) => next());
+
+router.get('/cards/', getCardsController);
+router.post('/cards/', createCardController);
+router.delete('/cards/:id', deleteCardController);
+
+router.all('/error', errorController);
+
+// logger
+app.use(async (ctx, next) => {
+	const start = new Date();
+	await next();
+	const ms = new Date() - start;
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// error handler
+app.use(async (ctx, next) => {
+	try {
+		await next();
+	} catch (err) {
+		console.log('Error detected', err);
+		ctx.status = err instanceof ApplicationError ? err.status : 500;
+		ctx.body = `Error [${err.message}] :(`;
+	}
+});
+
+// Создадим модель Cards на уровне приложения и проинициализируем ее
+app.use(async (ctx, next) => {
+	ctx.CardsModel = new CardsModel();
+	await ctx.CardsModel.loadFile();
+	await next();
+});
+
+app.use(bodyParser);
+app.use(router.routes());
+app.use(serve('./public'));
+
+app.listen(3000, () => {
+	console.log('Application started');
+});
---
+++ source/controllers/error.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = async () => {
+	throw new Error('Oops!');
+};
